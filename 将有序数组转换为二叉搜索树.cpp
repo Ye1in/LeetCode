933a7184eaa9de38fd1cb6b220da6d5e553700cd
Ye1in <199609153@qq.com>
2018-07-21 13:14:02 +0800

    7.21 push failed

--- 将有序数组转换为二叉搜索树.cpp
+++ 将有序数组转换为二叉搜索树.cpp
@@ -30,7 +30,17 @@ class Solution
   public:
     TreeNode *sortedArrayToBST(vector<int> &nums)
     {
-        
+        return sortedArrayToBST(nums, 0, nums.size() - 1);
+    }
+    TreeNode *sortedArrayToBST(vector<int> &nums, int begin, int end)
+    {
+        if (begin > end)
+            return NULL;
+        int mid = (begin + end) / 2;
+        TreeNode *root = new TreeNode(nums[mid]);
+        root->left = sortedArrayToBST(nums, begin, mid - 1);
+        root->right = sortedArrayToBST(nums, mid + 1, end);
+        return root;
     }
 };
 void trimLeftTrailingSpaces(string &input)
